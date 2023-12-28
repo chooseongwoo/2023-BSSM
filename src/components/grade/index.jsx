902dedb87a4de8d9cf9fbc14a0ebfe54f2603ffc
ChooSeongWoo <sldoal2423@gmail.com>
2023-12-28 09:27:23 +0900

    add :: 성적계산기

--- src/components/grade/index.jsx
+++ src/components/grade/index.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
 import "./style.css";
 
 const Grade = () => {
@@ -18,6 +16,81 @@ const Grade = () => {
   const rightTopSubject = ["실용 국어", "실용 영어"];
   const rightBottomSubject = ["체육", "음악", "디자인 일반"];
 
+  const [scores, setScores] = useState({
+    meister: 0,
+    subjects: {
+      수학: "A",
+      통합사회: "A",
+      통합과학: "A",
+      "컴퓨터 구조": "A",
+      "전기 전자 기초": "A",
+      "성공적인 직업생활": "A",
+      프로그래밍: "A",
+      "실용 국어": "A",
+      "실용 영어": "A",
+      체육: "A",
+      음악: "A",
+      "디자인 일반": "A",
+    },
+  });
+
+  const calculateScore = () => {
+    let totalScore = 0;
+
+    for (const subject in scores.subjects) {
+      const grade = scores.subjects[subject];
+      const credit = getCreditsForSubject(subject);
+      const gradeMultiplier = getGradeMultiplier(grade);
+
+      totalScore += gradeMultiplier * credit;
+    }
+
+    // Add meister score
+    totalScore += parseInt(scores.meister, 10) || 0;
+
+    console.log("총 점수는", totalScore, "입니다.");
+
+    // Save the result to local storage
+    localStorage.setItem("totalScore", totalScore);
+  };
+
+  const getCreditsForSubject = (subject) => {
+    const creditMap = {
+      수학: 6,
+      통합사회: 6,
+      통합과학: 6,
+      "컴퓨터 구조": 4,
+      "전기 전자 기초": 2,
+      "성공적인 직업생활": 2,
+      프로그래밍: 10,
+      "실용 국어": 4,
+      "실용 영어": 4,
+      체육: 2,
+      음악: 4,
+      "디자인 일반": 4,
+    };
+
+    return creditMap[subject] || 0;
+  };
+
+  const getGradeMultiplier = (grade) => {
+    const gradeMap = {
+      A: 5,
+      B: 4,
+      C: 3,
+      D: 2,
+      E: 1,
+    };
+
+    return gradeMap[grade] || 0;
+  };
+
+  const handleGradeChange = (subject, grade) => {
+    const newScores = { ...scores };
+    newScores.subjects[subject] = grade;
+    setScores(newScores);
+  };
+
   return (
     <div className="grade-container">
       <div className="grade">
@@ -30,9 +103,16 @@ const Grade = () => {
                   <td>{subject}</td>
                   <td>
                     <form className="grade-form">
-                      <select>
+                      <select
+                        onChange={(e) =>
+                          handleGradeChange(subject, e.target.value)
+                        }
+                        value={scores.subjects[subject]}
+                      >
                         {grades.map((grade) => (
-                          <option key={grade}>{grade}</option>
+                          <option key={grade} value={grade}>
+                            {grade}
+                          </option>
                         ))}
                       </select>
                     </form>
@@ -52,9 +132,14 @@ const Grade = () => {
                     <td>{subject}</td>
                     <td>
                       <form className="grade-form">
-                        <select>
+                        <select
+                          onChange={(e) =>
+                            handleGradeChange(subject, e.target.value)
+                          }
+                          value={scores.subjects[subject]}
+                        >
                           {grades.map((grade) => (
-                            <option value={grade} key={grade}>
+                            <option key={grade} value={grade}>
                               {grade}
                             </option>
                           ))}
@@ -75,9 +160,16 @@ const Grade = () => {
                     <td>{subject}</td>
                     <td>
                       <form className="grade-form">
-                        <select>
+                        <select
+                          onChange={(e) =>
+                            handleGradeChange(subject, e.target.value)
+                          }
+                          value={scores.subjects[subject]}
+                        >
                           {grades.map((grade) => (
-                            <option key={grade}>{grade}</option>
+                            <option key={grade} value={grade}>
+                              {grade}
+                            </option>
                           ))}
                         </select>
                       </form>
@@ -87,12 +179,23 @@ const Grade = () => {
               </tbody>
             </table>
             <p className="grade-title">마이스터 역량 인증제</p>
-            <input type="number" min={0} name="meister" max={150}/>
+            <input
+              type="number"
+              min={0}
+              name="meister"
+              max={150}
+              value={scores.meister}
+              onChange={(e) =>
+                setScores({ ...scores, meister: e.target.value })
+              }
+            />
           </div>
         </div>
       </div>
       <Link to="/score">
-        <button className="check">점수 확인</button>
+        <button className="check" onClick={calculateScore}>
+          점수 확인
+        </button>
       </Link>
     </div>
   );
